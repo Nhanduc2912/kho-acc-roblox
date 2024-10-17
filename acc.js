9aee923e11e6650b72858c62ff3cbbb3641afe93
Nhanduc2912 <nhanduc29122008@gmail.com>
2024-10-17 08:19:34 +0700

    init

---
+++ acc.js
@@ -0,0 +1,8 @@
+function pasuser(form) { 
+    if (form.id.value=="accadmin") { 
+    if (form.pass.value=="freefiremaichay2000") { location="admin.html" }
+    if (alert("WELLCOM TO LOGIN")) { location="admin.html" }
+    }
+    else { alert("sai Password") } 
+    
+    } 
---
+++ key.js
@@ -0,0 +1,7 @@
+function pasuser(form) { 
+    if (form.id.value=="tkbuy") { 
+    if (form.pass.value=="key1allacc",alert("WELLCOM TO LOGGIN"))
+    { location="main.html" }
+    }
+    else { alert("sai Password") } 
+    } 
